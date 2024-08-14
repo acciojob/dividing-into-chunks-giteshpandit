c39746e004e9e35ed41aaf2dd1311b0aa221b903
giteshpandit <panditgitesh38@gmail.com>
2024-08-14 16:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
-	“function divide(arr, n) {
-let currentSubArray=[];
-let currentSum = 0;
-let result = [];
-for (let item in arr) {
-    if(currentSum + item <= n) { 0+3 < 6 | 3 +1 < 6
-            currentSubArray.push(item); [0, 3, 1] //  currentSubArray when the loop closes
-            currentSum += item; [0+3+1] // Last iteration ends here
+  let currentSubArray = [];
+  let currentSum = 0;
+  let result = [];
+
+  for (let item of arr) {
+    if (currentSum + item <= n) {
+      currentSubArray.push(item);
+      currentSum += item;
     } else {
-        result.push(currentSubArray); // This line which returns the final output after collecting all sub-arrays is not executed
-        currentSubArray = [item];
-        currentSum=item;
+      result.push(currentSubArray);
+      currentSubArray = [item];
+      currentSum = item;
     }
-    if(currentSubArray.length > 0) {
-        result.push(currentSubArray);
-    }
-}
-return result;
-}
-”
+  }
+
+  // Push the last subarray if it has any elements
+  if (currentSubArray.length > 0) {
+    result.push(currentSubArray);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
+const n = parseInt(prompt("Enter n: "), 10);
 alert(JSON.stringify(divide(arr, n)));
+
